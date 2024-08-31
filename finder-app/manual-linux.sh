fb4eddae1f90748535b7dbea3547c6822dc1678e
sushant_p <sushant_potdar@hotmail.com>
2024-08-31 02:14:18 -0700

    assignment 3 part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,9 +35,27 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    #mrproper i.e. clean command
+    echo "RUNNING COMMAND --> clean aka mrproper"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+
+    #defconfig
+    echo "RUNNING COMMAND --> make defconfig"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+
+    #vmlinux target
+    echo "RUNNING COMMAND --> make all"
+    make -j32 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+
+    #modules and devicetree
+    echo "RUNNING COMMAND --> make modules and devicetrees"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -48,6 +66,12 @@ then
 fi
 
 # TODO: Create necessary base directories
+echo "RUNNING COMMAND --> mkdir basedirctorties" 
+mkdir ${OUTDIR}/rootfs 
+cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +80,65 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    # echo "RUNNING COMMAND --> configure busybox-> distclean ->defconfig" 
+     make distclean
+     make defconfig
+
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# # TODO: Make and install busybox
+ echo "RUNNING COMMAND --> make busybox" 
+ make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+ make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
-echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+ cd ${OUTDIR}/rootfs
+ echo "Library dependencies"
+ ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
+ ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# # TODO: Add library dependencies to rootfs
+echo "RUNNING COMMAND -->Copying lib files from arm sysroot to rootfd"
+armsysroot=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp -L ${armsysroot}/lib/ld-linux-aarch64.* -t ${OUTDIR}/rootfs/lib #program interpreter
+cp -L ${armsysroot}/lib64/libm.* -t ${OUTDIR}/rootfs/lib64
+cp -L ${armsysroot}/lib64/libresolv.* -t ${OUTDIR}/rootfs/lib64
+cp -L ${armsysroot}/lib64/libc.* -t ${OUTDIR}/rootfs/lib64
 
-# TODO: Make device nodes
 
-# TODO: Clean and build the writer utility
+# # TODO: Make device nodes
+echo "RUNNING COMMAND -->mknod for device nodes"
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Copy the finder related scripts and executables to the /home directory
-# on the target rootfs
+# # TODO: Clean and build the writer utility
+echo "RUNNING COMMAND -->clean and make finder app"
+assignments_finderapp=~/coursera_aeld/aeld_assignments/assignment-1-spotdar/finder-app
+cd ${assignments_finderapp}
+make clean #clean
+make CROSS_COMPILE=${CROSS_COMPILE}   #build
 
-# TODO: Chown the root directory
 
-# TODO: Create initramfs.cpio.gz
+# # TODO: Copy the finder related scripts and executables to the /home directory
+# # on the target rootfs
+echo "RUNNING COMMAND -->copy finder app related stuff to rootfs"
+cp -L ${assignments_finderapp}/autorun-qemu.sh -t ${OUTDIR}/rootfs/home/
+cp -L ${assignments_finderapp}/dependencies.sh -t ${OUTDIR}/rootfs/home/
+cp -L ${assignments_finderapp}/finder-test.sh -t ${OUTDIR}/rootfs/home/
+cp -L ${assignments_finderapp}/finder.sh -t ${OUTDIR}/rootfs/home/
+cp -r ${assignments_finderapp}/conf/ -t ${OUTDIR}/rootfs/home/
+cp -L ${assignments_finderapp}/writer ${OUTDIR}/rootfs/home/
+
+# # TODO: Chown the root directory
+echo "RUNNING COMMAND -->Chown the root directory"
+cd ${OUTDIR}/rootfs
+sudo chown -R root:root *
+
+# # TODO: Create initramfs.cpio.gz
+echo "RUNNING COMMAND -->Create initramfs.cpio.gz"
+cd ${OUTDIR}/rootfs
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ${OUTDIR}
+gzip -f initramfs.cpio
+echo "END OF manual-linux.sh SCRIPT"
